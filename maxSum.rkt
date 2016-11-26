a62b4d1066c292f03cfd0cacd4f8d8ec3263bfab
Tam Nguyen <genterist@gmail.com>
2016-11-25 22:12:06 -0600

    Develop 2nd version of MaxSum

--- maxSum.rkt
+++ maxSum.rkt
@@ -6,7 +6,7 @@
 ;
 ;This program will take a list and return
 ;the maximum sum value of a sublist among
-;all posssible sub-lists
+;all posssible CONSECUTIVE sub-lists
 ;-------
 
 (define (getList prompt)
@@ -16,8 +16,8 @@
 ( define original-list (getList "Enter a list: "))
 
 
-; this function sums all members of a list
-(define (sumIt lst)
+; this function finds the maximum sum of any possible consecutive sub list
+(define (maxSum lst)
   (let loop ((lst lst) (accum 0))
     (cond
       ((empty? lst) accum)
@@ -28,33 +28,6 @@
 )
 
 
-; this function find the largest number within a list
-(define (findMaxNum lst)
-  (foldl (lambda (e r) (if (or (not r) (> e r)) e r))
-         #f
-         lst))
-
-(display "The original list is : ")
-(display original-list)
-(display "\n")
-
-
-;this function finds the maximum sublist within a list
-;and return the sum of all of its members
-( define posList (filter positive? original-list))
-(cond
-  ((> (length posList) 0) ;if we can find a sub list of all possitive numbers
-      (display "The maximum list is : ")
-      (display posList)
-      (display "\nThe maximum sum is : ")
-      (display (sumIt posList))
-  )
-  (else ; if not, return the max single negative number
-      (display "The maximum list is the largest negative number : ")
-      (display (findMaxNum original-list))
-  )
-)
-(display "\n")
 
 
 
